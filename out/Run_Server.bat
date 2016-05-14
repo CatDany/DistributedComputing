59d51197d90b92d407247e76afccc386ecb5af86
Dany <dany2001ru@gmail.com>
2016-05-14 20:06:53 +0600

    changed batch files for 1.0-a6

--- out/Run_Server.bat
+++ out/Run_Server.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Running as SERVER
 set /p BFport="Server port: "
-java -jar "bfdist_1.0-a5.jar" server %BFport% --enableDebugLogging
+java -jar "bfdist_1.0-a6.jar" server %BFport% --enableDebugLogging
 pause

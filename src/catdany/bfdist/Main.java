623dd8b6e0dfdfe827efd340d28b1c9e9d9a7048
Dany <dany2001ru@gmail.com>
2016-07-29 20:47:24 +0600

    2.2-a10

--- src/catdany/bfdist/Main.java
+++ src/catdany/bfdist/Main.java
@@ -36,11 +36,11 @@ public class Main
 	/**
 	 * Version in convention (major.minor-maintenance/build)
 	 */
-	public static final String VERSION_NAME = "2.2-a9";
+	public static final String VERSION_NAME = "2.2-a10";
 	/**
 	 * Datetime in <u>seconds</u> representing when this version was built
 	 */
-	public static final long VERSION_DATE = 1468516245L;
+	public static final long VERSION_DATE = 1469803422L;
 	
 	/**
 	 * Side that the program is running on (client/server)<br>
